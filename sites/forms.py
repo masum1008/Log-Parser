8c6091f79f59da318f65f0a9dac635305e8353b9
Md. Istiyak Ahmed <shuvrothpol@gmail.com>
2016-11-07 18:55:23 +0600

    asd

--- sites/forms.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'istiyak'
-
-from django import forms
-
-class SiteForm(forms.Form):
-    name = forms.CharField(label="site_name", required=True)
-    url = forms.CharField(label="site_url", required=True)
